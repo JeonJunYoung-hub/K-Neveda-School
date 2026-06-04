037027cfcf86c4f2317d17bf9f26df70817761a1
Junyoung Jeon <young011204@gmail.com>
2026-06-05 01:32:35 +0900

    Add new camp pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { DefaultLayout } from './components/layout/DefaultLayout';
 import { ApplyPage } from './pages/ApplyPage';
 import { CampIntroPage } from './pages/CampIntroPage';
 import { HomePage } from './pages/HomePage';
+import { MediaPage } from './pages/MediaPage';
+import { NoticesPage } from './pages/NoticesPage';
 import { ProgramDetailPage } from './pages/ProgramDetailPage';
 import { SchedulePage } from './pages/SchedulePage';
+import { TeamPage } from './pages/TeamPage';
 
 export function App() {
   const currentPath = window.location.pathname.replace(/\/$/, '') || '/';
@@ -17,6 +20,12 @@ export function App() {
       <ProgramDetailPage slug={programMatch[1]} />
     ) : currentPath === '/apply' ? (
       <ApplyPage />
+    ) : currentPath === '/notices' ? (
+      <NoticesPage />
+    ) : currentPath === '/media' ? (
+      <MediaPage />
+    ) : currentPath === '/team' ? (
+      <TeamPage />
     ) : (
       <HomePage />
     );
