5586c22625ab6d484325f9535509404f2051691d
Junyoung Jeon <young011204@gmail.com>
2026-06-05 18:20:46 +0900

    Import PC layout styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { App } from './App';
 import './styles/global.css';
 import './styles/codex-overrides.css';
 import './styles/home-program-info.css';
+import './styles/desktop-pc-layout.css';
+import './styles/detail-pages.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
